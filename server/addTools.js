f9f8c09bd35f335c6cf35c5f6fe61d6d156debed
Stanislav Iliev <gigobest2@gmail.com>
2016-04-05 13:28:13 +0300

    Update addTools.js

--- server/addTools.js
+++ server/addTools.js
@@ -9,6 +9,7 @@ module.exports = {
 			indexName: 'angularjs',
 			description: "AngularJS is a structural framework for dynamic web apps. It lets you use HTML as your template language and lets you extend HTML's syntax to express your application's components clearly and succinctly. Angular's data binding and dependency injection eliminate much of the code you would otherwise have to write. And it all happens within the browser, making it an ideal partner with any server technology.",
 			link: 'https://angularjs.org/',
+			language: 'JavaScript',
 			image: 'angularjs.jpg'
 		});
 
@@ -17,6 +18,7 @@ module.exports = {
 			indexName: 'express',
 			description: "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications. ",
 			link: 'http://expressjs.com/',
+			language: 'JavaScript',
 			image: 'expressjs.jpg'
 		});
 
@@ -25,6 +27,7 @@ module.exports = {
 			indexName: 'mongodb',
 			description: "MongoDB is an open source, document-oriented database designed with both scalability and developer agility in mind. Instead of storing your data in tables and rows as you would with a relational database, in MongoDB you store JSON-like documents with dynamic schemas.",
 			link: 'https://www.mongodb.org/',
+			language: 'Other',
 			image: 'mongodb.jpg'
 		});
 
@@ -33,7 +36,62 @@ module.exports = {
 			indexName: 'nodejs',
 			description: "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. ",
 			link: 'https://nodejs.org/en/',
+			language: 'JavaScript',
 			image: 'nodejs.jpg'
+		});
+		
+		Tool.create({
+			name: 'APS.NET',
+			indexName: 'aspnet',
+			description: 'ASP.NET 5 is a new open-source, cross-platform, high performance and lightweight framework for building Web Applications using .NET.',
+			link: 'http://www.asp.net/',
+			language: 'C#',
+			image: 'http://phoenixcoded.com/images/mvc_logo1.png'
+		});
+		
+		Tool.create({
+			name: 'Ruby on Rails',
+			indexName: 'rubyonrails',
+			description: 'Rails is a web application development framework written in the Ruby language. It is designed to make programming web applications easier by making assumptions about what every developer needs to get started. It allows you to write less code while accomplishing more than many other languages and frameworks.',
+			link: 'http://rubyonrails.org/',
+			language: 'Ruby',
+			image: 'http://www.xtreemsolution.com/img/rubyrails.png'
+		});
+		
+		Tool.create({
+			name: 'Django',
+			indexName: 'django',
+			description: 'Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of Web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.',
+			link: 'https://www.djangoproject.com/',
+			language: 'Python',
+			image: 'https://www.djangoproject.com/s/img/logos/django-logo-negative.png'
+		});
+		
+		Tool.create({
+			name: 'Meteor',
+			indexName: 'meteor',
+			description: 'Meteor is a full-stack JavaScript platform for developing modern web and mobile applications. Meteor includes a key set of technologies for building connected-client reactive applications, a build tool, and a curated set of packages from the Node.js and general JavaScript community.',
+			link: 'https://www.meteor.com/',
+			language: 'JavaScript',
+			image: 'http://blog.thesecretlab.nl/wp-content/uploads/2015/07/meteor-logo.png'
+		});
+		
+		Tool.create({
+			name: 'Laravel',
+			indexName: 'laravel',
+			description: 'Laravel is a free, open-source PHP web framework, intended for the development of web applications following the model–view–controller (MVC) architectural pattern. Some of the features of Laravel are a modular packaging system with a dedicated dependency manager, different ways for accessing relational databases, utilities that aid in application deployment and maintenance, and its orientation toward syntactic sugar.',
+			link: 'https://laravel.com/',
+			language: 'PHP',
+			image: 'http://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2015/02/1423519219laravel-l-slant.png'
+		});
+		
+		Tool.create({
+			name: 'Spring',
+			indexName: 'spring',
+			description: 'The Spring Framework is a Java platform that provides comprehensive infrastructure support for developing Java applications. Spring handles the infrastructure so you can focus on your application.',
+			link: 'http://spring.io/',
+			language: 'Java',
+			image: 'https://spring.io/img/spring-by-pivotal.png'
 		});
 	}
 }
