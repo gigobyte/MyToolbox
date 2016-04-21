e9d1b20879549178e0d3d7c567323a3e7db097c6
gigobyte <gigobest2@gmail.com>
2016-04-21 18:01:33 +0300

    Loading

---
+++ webapp/scripts/components/seLoadingDirective.js
@@ -0,0 +1,37 @@
+angular.module('mytoolboxApp').directive('seLoading', function($parse, $timeout) {
+	'use strict';
+	var CLASS_NAME = 'se-loading';
+	var TIMEOUT = 0;
+	return {
+		restrict: 'A',
+		link: function postLink(scope, element, attrs) {
+			var promise;
+
+			function hideWaiter() {
+				if (promise) {
+					$timeout.cancel(promise);
+					promise = null;
+				}
+
+				element.removeClass(CLASS_NAME);
+			}
+			function showWaiter() {
+				hideWaiter();
+				promise = $timeout(function() {
+					element.addClass(CLASS_NAME);
+				}, TIMEOUT);
+			}
+			scope.$watch(attrs.seLoading, function(value) {
+				console.log(value);
+				if (value) {
+					hideWaiter();
+				} else {
+					showWaiter();
+				}
+			});
+			scope.$on('$destroy', function() {
+				hideWaiter();
+			});
+		}
+	};
+});
