81c31a0824f6930d3fe1f54aac7f1903b26653c4
gigobyte <gigobest2@gmail.com>
2016-04-22 23:49:54 +0300

    Settings page

--- webapp/scripts/user/settings/settingsCtrl.js
+++ webapp/scripts/user/settings/settingsCtrl.js
@@ -1,9 +1,15 @@
-angular.module('mytoolboxApp').controller('UserSettingsCtrl', function (UserSettingsService) {
+angular.module('mytoolboxApp').controller('UserSettingsCtrl', function (UserSettingsService, AuthenticationService, $state) {
 	'use strict';
 
 	var controller = this;
 
 	function initState() {
+		AuthenticationService.getLoggedUser().then(function(res) {
+			if(!res) {
+				$state.go('home.first', null, {reload: true});
+			}
+			controller.user = res;
+		});
 	}
 
 	function attachMethods() {
